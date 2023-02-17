5261bd1519fc8016beebdedfea5af00289519222
Wanyu Jiang <wanyu19930930@gmail.com>
2023-02-17 12:25:02 +0900

    added platform to bundle lock

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, presence: true, uniqueness: { scope: :list }
+  validates :list, presence: true, uniqueness: { scope: :movie }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'movie/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# lists
+# index, show, new, create
+# bookmarks
+# new, create, destroy
